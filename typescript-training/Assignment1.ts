b1e98dc319f5b68f0445f0e8f2cecb2822e4a81d
jyothi1812 <nagajyoreddy@gmail.com>
2026-02-12 20:15:43 +0530

    Assignment1

---
+++ typescript-training/Assignment1.ts
@@ -0,0 +1,45 @@
+let temp:number = 25.5;
+console.log("Tempature:",temp);
+let orderIsplaced:boolean = true;
+console.log("orderIsplaced:", orderIsplaced);
+let orderIsnotplaced:boolean = false;
+console.log("orderIsnotplaced:", orderIsnotplaced);
+let phonenumber:number = 1234567890;
+console.log("phonenumber:",phonenumber);
+let amount:number = 1000.50;
+console.log("BankAmount:",amount);
+let emailId:string = "john.doe@example.com";
+console.log("EmailID:",emailId);
+let coordinate:(string|number)[]  = ["Longitute",37.7749, "Latitute",-122.4194];
+console.log("coordinate:",coordinate);
+let maritalstatus:boolean = true;
+console.log("IsMarried:",maritalstatus);
+let single:boolean = false;
+console.log("isSingle:",single);
+let occupation:string = "software engineer";
+console.log("EmployeeOccupation:",occupation);
+let colour:string = "Blue";
+console.log("Favcolour:",colour);
+let followerscount:number = 10000000;
+console.log("noOffolloweresinsocialmedia:",followerscount);
+let movieRating:number = 7.5;
+console.log("MovieRating:",movieRating);
+let bloodgroup:string ="A+";
+console.log("BloodGroup",bloodgroup);
+let titleoftheBook:string = "To Kill a Mockingbird";
+console.log("BoolTitle:",titleoftheBook);
+let noOfEmployees:number = 500;
+console.log("TotalnoOfEmployess:",noOfEmployees);
+const timeofEvent:string = "2.30pm"
+console.log("CurrrentTime:", timeofEvent);
+let countryName:string = "UnitedStates";
+console.log("CountryName:",countryName);
+let personEyeColour:string = "Brown";
+console.log("EyeColour:",personEyeColour);
+let birthplace:string = "NewYorkcity";
+console.log("BirthPlace:",birthplace);
+let distance:number = 200.5
+console.log("Distance:",distance);
+    
+
+
