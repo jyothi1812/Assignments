196753b5caf2eb254d99789b8a3f6f0df9592ea3
jyothi1812 <nagajyoreddy@gmail.com>
2026-02-15 23:56:08 +0530

    Assignment2 operators

--- typescript-training/Assignment2.ts
+++ typescript-training/Assignment2.ts
@@ -1,83 +1,64 @@
 //Operators
+function isEligibleForLoan(
+    customerName:string,
+    creditScore:number,
+    customerIncome:number,
+    isEmployed:boolean,
+    debitToIncomeRatio:number): 
+    void{
 
-//1.credit score
-let creditScore:number = 700;
+        console.log("Loan eligible status for:" +customerName);
+    
+//let creditScore:number = 700;
 if(creditScore >750)
 {
-    console.log("Loan is approved");}
- else if( creditScore >= 650 && creditScore <=750)
-    {console.log("Additional checks are performed")}
-    else{creditScore<650
-        {
-            console.log("Loan is denied");}
-        }
-
-2. //Income
-let customerIncome:number = 42000;
-if(
-creditScore>=650 && creditScore<=750 && customerIncome >= 50000)
+console.log("Loan is approved");
+}else if( creditScore >= 650 )
     {
-   console.log("Loan is considered");
-}
-else
-{
-    console.log("Loan is not considered");
-}
-
-// 3 Employment status
-let employeStatus:string =(customerIncome >= 50000)? "employed " :"unemployed" ;
-console.log(employeStatus);
-if(employeStatus == "unemployed")
-console.log("loan is denied")
-
-// Debt to Income Ratio
-
-let isemployed:boolean = true;
-let debitRatio:number = 40.5;
-
-if(isemployed)
-{
-    if(debitRatio > 30)
-        {
-        console.log("Debit ratio is accepted");
-        }
-        else 
+    
+    if(customerIncome >= 50000)
+    {
+        if(isEmployed)
             {
-            console.log("Debit ratio is not accepted");
-            }
-        }
-        else
-        {
-            console.log("Employee is unemployed, debit is not performed");
-        }
-
-        if(debitRatio < 40)
-        {
-            console.log("loan is approved");
-        }
-        if(debitRatio >= 40)
-        {
-            console.log("loan is denied");
-        }
-
-    // Create the common function 
-    function isEligibleForLoan
-    (customerName:string,creditScore:number,customerIncome:number,
-        isemployed:boolean, debitRatio:number): void {
-            if(isemployed && creditScore > 750 && customerIncome >= 55000 && debitRatio >=35.0)
-            {
-                console.log('eligible for loan by'+ customerName);
+                if(debitToIncomeRatio <40.0)
+                {
+                    console.log("Loan approved")
+                }
+                else
+                {
+                    console.log(" Loan denied: debit to ratio is too high");
+                }
             }
             else
             {
-                console.log('not eligible for loan by'+ customerName);
-            }  
+                console.log("loan denied: customer is not employed")
+            }
         }
-isEligibleForLoan("John doe",800,60000,true,40.0);
-    
-         
-
         
+            else
+                {
+                console.log("loan denied: Income is Lessthan required");
+                }
+            }
+            else
+                {
+                console.log("Loan denied: credit score is too low");
+            }
+        }
+    
+    
+    
+         isEligibleForLoan
+         (
+            "john doe",
+            720,
+            55000.0,
+            true,
+            45.0
+         );
+         export{};
+        
+    
     
 
 
